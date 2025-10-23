7ce1463748f6e1a3b11026e6a41ffd46aa4bf8bc
Altair12-png <anwar.syadzali@gmail.com>
2025-10-23 16:36:31 +0700

    peminjaman.php

--- app/Models/Peminjaman.php
+++ app/Models/Peminjaman.php
@@ -9,27 +9,28 @@ class Peminjaman extends Model
 {
     use HasFactory;
 
-    protected $table = 'peminjaman';
+protected $table = 'peminjaman';
 
-    protected $fillable = [
-        'fasilitas_id',
-        'nama_peminjam',
-        'jumlah_pinjam',
-        'alasan',
-        'tanggal_pinjam',
-        'tanggal_kembali_rencana',
-        'tanggal_kembali_aktual',
-        'status_peminjaman',
-    ];
+protected $fillable = [
+    'fasilitas_id',
+    'nama_peminjam',
+    'jumlah_pinjam',
+    'alasan',
+    'tanggal_pinjam',
+    // Kolom yang digunakan oleh Backend
+    'tanggal_kembali_rencana', 
+    'tanggal_kembali_aktual',
+    'status_peminjaman',
+];
 
-    protected $dates = [
-        'tanggal_pinjam',
-        'tanggal_kembali_rencana',
-        'tanggal_kembali_aktual',
-    ];
+protected $dates = [
+    'tanggal_pinjam',
+    'tanggal_kembali_rencana', // Diperhatikan
+    'tanggal_kembali_aktual',
+];
 
-    public function fasilitas()
-    {
-        return $this->belongsTo(Fasilitas::class);
-    }
+public function fasilitas()
+{
+    return $this->belongsTo(Fasilitas::class);
+}
 }

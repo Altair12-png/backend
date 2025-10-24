de2e0128de4b465a89d323a45a29d421d70737d3
Altair12-png <anwar.syadzali@gmail.com>
2025-10-24 11:10:27 +0700

    fiks seedfasilitas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
        $this->call(UserSeeder::class);
+       
+       //Memanggil FasilitasSeeder
+       $this->call(FasilitasSeeder::class); 
     }
 }

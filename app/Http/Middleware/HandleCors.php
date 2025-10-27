c38349ee123255d1e732efb239564f736719e31d
Altair12-png <anwar.syadzali@gmail.com>
2025-10-27 13:02:42 +0700

    peminjamancontroller

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -8,9 +8,20 @@ class HandleCors
 {
     public function handle($request, Closure $next)
     {
+        // Jika request adalah preflight (OPTIONS)
+        if ($request->getMethod() === "OPTIONS") {
+            return response()->json('OK', 200, [
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type, Authorization, ngrok-skip-browser-warning, Accept, Origin',
+            ]);
+        }
+
+        // Untuk semua request normal
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, ngrok-skip-browser-warning, Accept, Origin')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }

df8d84813fc24ae2612ada5cf2fcc1be2bd15079
Altair12-png <anwar.syadzali@gmail.com>
2025-10-28 10:27:43 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Response;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/proxy-image/{filename}', function ($filename) {
+    $path = storage_path('app/public/fasilitas/' . $filename);
+    
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    $mime = mime_content_type($path);
+    return Response::make(file_get_contents($path), 200, [
+        'Content-Type' => $mime,
+        'Access-Control-Allow-Origin' => '*',
+    ]);
 });
